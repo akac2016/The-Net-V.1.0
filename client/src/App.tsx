38ba392bd8c183816731aefb25a2518ed311c2b2
Evan Coulson <evancoulson@wl-219-141.wpa.claremont.edu>
2020-02-17 23:19:53 -0800

    example code from meeting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,20 @@
 import React, { Props } from 'react';
-import Axios from "axios";
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 interface IState {
-	isPinging: boolean
-	isRunning: boolean
+	title: String
 }
 
 export default class App extends React.Component<{}, IState> {
 	public state: IState = {
-		isPinging: true,
-		isRunning: false,
+		title: "Title"
 	};
 
 	constructor(props : {}) {
 		super(props);
-		this.getStatus();
+		this.changeTitle = this.changeTitle.bind(this);
 	}
 
 	public render() {
@@ -25,22 +22,16 @@ export default class App extends React.Component<{}, IState> {
 			<div className="App">
 				<header className="App-header">
 					<FontAwesomeIcon icon={faCoffee} />
-					<p> The Net Project </p>
-					{ 
-						!this.state.isPinging 
-							? <p>Server is up.</p> 
-							: <p>Server is down.</p> 
-					}
+					<p> {this.state.title} </p>
+					<button onClick={this.changeTitle}>Click me!</button>
 				</header>
 			</div>
 		);
 	}
 
-	async getStatus() {
-		const res = await Axios.get("/ping");
+	changeTitle() {
 		this.setState({
-			isPinging: false,
-			isRunning: res.data.running
-		})
+			title: "The Net Project"
+		});
 	}
 }
